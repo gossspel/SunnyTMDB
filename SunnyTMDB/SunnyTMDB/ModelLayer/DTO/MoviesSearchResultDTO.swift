a7bc2be7385530f41333f55bf94e9ec055a05128
Sunny Chan <sunnyskchan@berkeley.edu>
2021-09-12 16:28:36 -0700

    Added MoviesSearchResultDTO.

---
+++ SunnyTMDB/SunnyTMDB/ModelLayer/DTO/MoviesSearchResultDTO.swift
@@ -0,0 +1,20 @@
+//
+//  MoviesSearchResultDTO.swift
+//  SunnyTMDB
+//
+//  Created by Sunny Chan on 9/12/21.
+//
+
+import Foundation
+
+struct MoviesSearchResultDTO: Codable {
+    let currentPage: Int
+    let totalPagesCount: Int
+    let movies: [MovieDTO]
+    
+    enum CodingKeys: String, CodingKey {
+        case currentPage = "page"
+        case totalPagesCount = "total_pages"
+        case movies = "results"
+    }
+}
