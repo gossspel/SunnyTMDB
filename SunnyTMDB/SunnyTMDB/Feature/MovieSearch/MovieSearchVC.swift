f7b6b0d0c37d8010799a73e42c368d7004efdf5a
Sunny Chan <sunnyskchan@berkeley.edu>
2021-09-13 16:40:36 -0700

    Added some updates.

---
+++ SunnyTMDB/SunnyTMDB/Feature/MovieSearch/MovieSearchVC.swift
@@ -0,0 +1,18 @@
+//
+//  MovieSearchVC.swift
+//  SunnyTMDB
+//
+//  Created by Sunny Chan on 9/13/21.
+//
+
+import UIKit
+
+class MovieSearchVC: UIViewController {
+    private let tableView: UITableView = UITableView(frame: .zero, style: .plain)
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
