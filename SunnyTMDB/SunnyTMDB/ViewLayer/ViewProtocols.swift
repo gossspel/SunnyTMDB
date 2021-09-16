75909be6a32946d68a89637e719c0b37c83db3ca
Sunny Chan <sunnyskchan@berkeley.edu>
2021-09-16 15:53:36 -0700

    Added more updates.

---
+++ SunnyTMDB/SunnyTMDB/ViewLayer/ViewProtocols.swift
@@ -0,0 +1,20 @@
+//
+//  ViewProtocols.swift
+//  SunnyTMDB
+//
+//  Created by Sunny Chan on 9/16/21.
+//
+
+import Foundation
+
+protocol LabelProtocol: AnyObject {
+    func updateLabelText(text: String?)
+}
+
+protocol ImageViewProtocol: AnyObject {
+    func updateImageByRemoteURL(imageURLStr: String?)
+}
+
+protocol CircularRingViewProtocol: AnyObject {
+    func updateRingFill(percentage: Float?, animated: Bool)
+}
